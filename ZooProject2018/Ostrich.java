0475ad8ae397b43dee289360810c57506750dfdb
ghostkh2020 <kumlachew12292000@gmail.com>
2018-11-19 08:55:42 -0500

    i made some change in my ostrich class

--- ZooProject2018/Ostrich.java
+++ ZooProject2018/Ostrich.java
@@ -7,5 +7,57 @@
  */
 public class Ostrich
 {
+    private String sound;
+    private int speed;
+    private int height;
+    
+    public Ostrich()
+    {
+        sound = "unkown";
+        speed = 0;
+        height = 0;
+    }
+    public Ostrich(String theSound, int theSpeed, int theHeight)
+    {
+        sound = theSound;
+        speed = theSpeed;
+        height = theHeight;
+    }
+    public String getSound()
+    {
+        return sound;
+    }
+    public int getSpeed()
+    {
+        return speed;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
+    public void setSound(String newSound)
+    {
+        sound = newSound;   
+    }
+    public void setSpeed(int newSpeed)
+    {
+        speed = newSpeed;
+    }
+    public void setHeight(int newHeight)
+    {
+        height = newHeight; 
+    }
+    public String fact()
+    {
+        return "ostrich is the largest bird in the world";
+    }
+    public String famous()
+    {
+        return "Ostrich carries about 1 kilogram (2.2 lb) of stones in its stomach.";
+    }
+    public String whyImportant()
+    {
+        return "Today, ostrich farms are considered to be among the most profitable agricultural projects";
+    }    
 
 }
