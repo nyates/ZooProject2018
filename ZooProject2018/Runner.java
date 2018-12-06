3c7927dd3687b4bf86ceb1a2df356264618d747d
herberthG <perezenrique362@gmail.com>
2018-12-06 08:52:12 -0500

    add interface

---
+++ ZooProject2018/Runner.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of interface Runner here.
+ *
+ * @author HGomez
+ * @version 2018/12/5
+ */
+public interface Runner
+{
+    public static final int MINIMU_RUNNER_SPEED = 1;
+    
+    public abstract String Run();
+    public abstract int getRunnerSpeed();
+}
