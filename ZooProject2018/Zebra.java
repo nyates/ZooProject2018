5d35ce236a40deb6f2d8eb5a209983b4b501e6b9
herberthG <perezenrique362@gmail.com>
2018-12-07 09:56:54 -0500

    add two new class

---
+++ ZooProject2018/Zebra.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class Zebra here.
+ *
+ * @author HGomez
+ * @version 2018-12-07
+ */
+public class Zebra
+{
+    private String species;
+    private String gender;
+    private String populationLocation;
+    private boolean weigh;
+    
+    public Zebra()
+    {
+   
+    }
+}
