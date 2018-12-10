11e1854d97e5e8a15e08b21387bc667a92145133
herberthG <perezenrique362@gmail.com>
2018-12-10 14:30:18 -0500

    add new class

---
+++ ZooProject2018/Fox.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Fox here.
+ *
+ * @author HGomez
+ * @version 2018/12/10
+ */
+public class Fox
+{
+    private String typesOfFox;
+    private String gender;
+    private int height;
+    private int weight;
+    
+    public Fox()
+    {
+    typesOfFox = "unkown";
+    gender = "unkown";
+    height = 50;
+    weight = 14;
+    }
+    
+    public Fox(String newTypesOfFox, String newGender, int newHeight, int newWeight)
+    {
+    typesOfFox = newTypesOfFox;
+    gender = newGender;
+    height = newHeight;
+    weight = newWeight;
+    }
+    
+    
+}
