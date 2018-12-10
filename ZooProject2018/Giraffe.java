b93995343e1c345011f60475aa3ab11d08e6d0ca
herberthG <perezenrique362@gmail.com>
2018-12-10 09:52:22 -0500

    to the classes i added getters and setters

--- ZooProject2018/Giraffe.java
+++ ZooProject2018/Giraffe.java
@@ -7,27 +7,82 @@
  */
 public class Giraffe
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Giraffe
-     */
+    private String typesOfGiraffes;
+    private String gender;
+    private int height;
+    private int weight;
+    
     public Giraffe()
     {
-        // initialise instance variables
-        x = 0;
+    typesOfGiraffes = "unkown";
+    gender = "unkown";
+    height = 6;
+    weight = 1192;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public Giraffe(String newTypesOfGiraffes, String newGender, int newHeight, int newWeight)
     {
-        // put your code here
-        return x + y;
+    typesOfGiraffes = newTypesOfGiraffes;
+    gender = newGender;
+    height = newHeight;
+    weight = newWeight;
+    }
+    
+    public String getTypes()
+    {
+        return typesOfGiraffes;
+    }
+    
+    public void setTypes(String newTypes)
+    {
+        typesOfGiraffes = newTypes;
+    }
+    
+    public String getGender()
+    {
+        return gender;
+    }
+    
+    public void setGender(String newGender)
+    {
+        gender = newGender;
+    }
+    
+    public int getHeight()
+    {
+        return height;
+    }
+    
+    public void setHeight(int newHeight)
+    {
+        height = newHeight;
+    }
+    
+    public int getWeight()
+    {
+        return weight;
+    }
+    
+    public void setWeight(int newWeight)
+    {
+        weight = newWeight;
+    }
+    
+    
+    
+    
+    public String eat()
+    {
+        return "They feed mainly on acacia trees and Commiphora (myrrh).";
+    }
+    
+    public String howMuchDecreasedTheGiraffePopulation()
+    {
+        return "The world population of giraffes has decreased by almost 40% in just 30 years.";
+    }
+    
+    public String howManyYearsHeLives()
+    {
+        return "Both male and female can live up to about 25 years.";
     }
 }
--- ZooProject2018/Zebra.java
+++ ZooProject2018/Zebra.java
@@ -7,13 +7,82 @@
  */
 public class Zebra
 {
-    private String species;
+    private String typesOfZebra;
     private String gender;
-    private String populationLocation;
-    private boolean weigh;
+    private int  populationLocation;
+    private int weigh;
     
     public Zebra()
     {
-   
+    typesOfZebra = "unkown";
+    gender = "unkown";
+    populationLocation = 9;
+    weigh = 400;
+    }
+    
+    public Zebra(String newTypesOfZebra, String newGender, int newPopulationLocation, int newWeigh)
+    {
+    typesOfZebra = newTypesOfZebra;
+    gender = newGender;
+    populationLocation = newPopulationLocation;
+    weigh = newWeigh;
+    }
+    
+    public String getTypes()
+    {
+        return typesOfZebra;
+    }
+    
+    public void setTypes(String newTypes)
+    {
+        typesOfZebra = newTypes;
+    }
+    
+    public String getGender()
+    {
+        return gender;
+    }
+    
+    public void setGender(String newGender)
+    {
+       gender = newGender; 
+    }
+    
+    public int getPopulationLocation()
+    {
+        return populationLocation;
+    }
+    
+    public void setPopulationLocation(int newPopulationLocation)
+    {
+        populationLocation = newPopulationLocation;
+    }
+    
+    public int getWeigh()
+    {
+        return weigh;
+    }
+    
+    public void setWeigh(int newWeigh)
+    {
+        weigh = newWeigh;
+    }
+    
+    
+    
+    
+    public String eat()
+    {
+        return "They feed on coarse grass, leaves and buds";
+    }
+    
+    public String howManyZebraAreThereInTheWorld()
+    {
+        return "In general, the number of zebras plains is around 750,000.";
+    }
+    
+    public String howManyLiveAZebra()
+    {
+        return "The life expectancy of the zebra is approximately 30 years.";
     }
 }
